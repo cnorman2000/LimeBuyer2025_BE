de4119c970e3d5dea7099d22bb34da0566da7e5d
cnorman2000 <cnorman2000@gmail.com>
2025-07-08 11:36:54 +0100

    store and user data expanded

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -29,4 +29,274 @@ module.exports = [
     avatar_url:
       "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
   },
+  // {
+  //   uid: 6,
+  //   username: "PeachyKeen",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 7,
+  //   username: "BananaBandit",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 8,
+  //   username: "MangoMamba",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 9,
+  //   username: "PineapplePunk",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 10,
+  //   username: "BerryBlaster",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 11,
+  //   username: "CitrusKnight",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 12,
+  //   username: "FiggyFresh",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 13,
+  //   username: "KiwiWhiz",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 14,
+  //   username: "PapayaPal",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 15,
+  //   username: "MelonManiac",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 16,
+  //   username: "AvocadoAmbush",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 17,
+  //   username: "CherryChaser",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 18,
+  //   username: "AppleAnomaly",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 19,
+  //   username: "GrapefruitGuru",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 20,
+  //   username: "BlueberryBeast",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 21,
+  //   username: "FruitNinja2.0",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 22,
+  //   username: "DurianDaredevil",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 23,
+  //   username: "TangerineTornado",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 24,
+  //   username: "ZestyZebra",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 25,
+  //   username: "BerryBuff",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 26,
+  //   username: "CoconutCoyote",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 27,
+  //   username: "StrawberryStorm",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 28,
+  //   username: "DragonfruitDream",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 29,
+  //   username: "GuavaGoose",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 30,
+  //   username: "PlumPirate",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 31,
+  //   username: "LimeLurker",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 32,
+  //   username: "CranberryCreep",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 33,
+  //   username: "PearProphet",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 34,
+  //   username: "JackfruitJuggler",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 35,
+  //   username: "ApricotAgent",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 36,
+  //   username: "PassionFruitPunk",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 37,
+  //   username: "PersimmonPhantom",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 38,
+  //   username: "MulberryMystic",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 39,
+  //   username: "RaspberryRogue",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 40,
+  //   username: "LycheeLegend",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 41,
+  //   username: "SatsumaSeeker",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 42,
+  //   username: "GooseberryGiant",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 43,
+  //   username: "RambutanRebel",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 44,
+  //   username: "OliveOracle",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 45,
+  //   username: "FruitFreak",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 46,
+  //   username: "CurrantCommander",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 47,
+  //   username: "StarfruitSniper",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 48,
+  //   username: "NectarineNinja",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 49,
+  //   username: "Bananarchist",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
+  // {
+  //   uid: 50,
+  //   username: "FruitFiestaFan",
+  //   avatar_url:
+  //     "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+  // },
 ];
